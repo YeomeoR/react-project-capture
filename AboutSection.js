fca8f7495361649226517d0f56e87d2e6ccb9e75
YeomeoR <76974577+YeomeoR@users.noreply.github.com>
2021-01-05 21:17:19 +0000

    Styled Components

--- AboutSection.js
+++ AboutSection.js
@@ -1,35 +1,69 @@
 import React from 'react';
 
 import home1 from '../images/home1.png';
+// styled components!!!!!!!!!!!!! AWESOME!
+import styled from 'styled-components';
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <StyledAbout>
+      <StyledDescription>
         <div className="title">
-          <div className="hide">
+          <StyledHide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide>
             <h2>true.</h2>
-          </div>
+          </StyledHide>
         </div>
         <p>
-          Contact us for any ideas that you have. We have professionals with
-          amazing skills.
+          Contact us for any ideas that you have. We can turn your dreams into
+          reality.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </StyledDescription>
+      <StyledImage>
         <img src={home1} alt="Guy with a camera" />
-      </div>
-    </div>
+      </StyledImage>
+    </StyledAbout>
   );
 };
+
+// styled components
+const StyledAbout = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem;
+  color: white;
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const StyledHide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
